f1445bc6973416066e470b9bd863f8a18540900e
aocha <ao.hk215@gmail.com>
2020-08-09 17:23:54 +0900

    Add Problem0504.java to chap05

---
+++ src/chap05/Problem0504.java
@@ -0,0 +1,15 @@
+package chap05;
+
+import java.util.Arrays;
+
+public class Problem0504 {
+  public static void main(String[] args) {
+    System.out.println(Math.pow(6, 3));
+
+    System.out.println(Math.abs(-15));
+
+    int[] array = new int[] { 110, 14, 28, 32 };
+    Arrays.sort(array);
+    System.out.println(Arrays.toString(array));
+  }
+}
