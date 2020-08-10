ed14f6bc43d09b32d656efe8259249d70bc6396b
aocha <ao.hk215@gmail.com>
2020-08-10 21:32:32 +0900

    Add Practice0601.java to chap06

---
+++ src/chap06/Practice0601.java
@@ -0,0 +1,11 @@
+package chap06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Practice0601 {
+  public static void main(String[] args) {
+    var list = new ArrayList<Integer>(Arrays.asList(16, 24, 30, 39));
+    System.out.println(list.toString());
+  }
+}
